33dd77c601534e30e183995442645b8d8a18d903
Natan Camargo Rodrigues <natanrodrigues0044@gmail.com>
2025-01-10 20:28:38 +1000

    Adding Xcode project

---
+++ Posture/PostureTests/PostureTests.swift
@@ -0,0 +1,16 @@
+//
+//  PostureTests.swift
+//  PostureTests
+//
+//  Created by Natan Camargo Rodrigues on 10/1/2025.
+//
+
+import Testing
+
+struct PostureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
